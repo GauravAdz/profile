3ecb5dc2bbd8d7a9d61feea90fff78ad22390044
GauravAdz <108446217+GauravAdz@users.noreply.github.com>
2023-11-23 22:40:48 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  // You can add code here to handle form submission, e.g., sending data to a server.
-  // For a simple example, let's just log the form data to the console.
-  const formData = new FormData(event.target);
-  const data = {};
-  formData.forEach((value, key) => {
-    data[key] = value;
-  });
-  console.log("Form Data:", data);
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
